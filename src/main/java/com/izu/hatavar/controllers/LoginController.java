68d7ef478872a853adadab5e4c32203d78c60537
mekin <muhammednurekin@gmail.com>
2018-05-29 16:13:48 +0300

    initial Commit Signup Works.

---
+++ src/main/java/com/izu/hatavar/controllers/LoginController.java
@@ -0,0 +1,23 @@
+package com.izu.hatavar.controllers;
+
+import com.izu.hatavar.models.User;
+import com.izu.hatavar.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class LoginController
+{
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody User user)
+    {
+        userService.save(user);
+    }
+}
